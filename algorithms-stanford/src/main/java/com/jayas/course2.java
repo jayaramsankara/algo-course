3eaccaf487d5de24c30ac8f59acf47e04cd07092
jayas <jayas@cisco.com>
2022-11-18 10:18:19 +0530

    Week2 assignment done for course 2

---
+++ algorithms-stanford/src/main/java/com/jayas/course2.java
@@ -0,0 +1,471 @@
+package com.jayas;
+
+
+
+
+
+
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ */
+public class course2 {
+
+    private static boolean[] visitedVertices = null;
+    private static List<Boolean> visitedVerticesList = null;
+    private static TreeMap<Integer,Integer> finishTimes = null;
+    private static Map<Integer,Integer> revFinishTimes = null;
+    private static Map<Integer, List<Set<Integer>>> leaders = new HashMap<>();
+    private static List sccSizes = new LinkedList();
+
+    private static  class Pair<T,V> {
+        private T first;
+        private V second;
+
+        public Pair(T first, V second) {
+            this.first =first;
+            this.second = second;
+        }
+
+        public T getFirst(){
+            return first;
+        }
+
+        public V getSecond() {
+            return second;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + first +
+                    "," + second +
+                    ")";
+        }
+    }
+    private static class Graph {
+        @Override
+        public String toString() {
+            return "Graph{" +
+                    "edges=" + edges +
+                    '}';
+        }
+
+        private Map<Integer, List<Pair<Integer,Integer>>> edges = new LinkedHashMap<>();
+
+
+        public Graph() {
+
+        }
+
+        public int numVertices() {
+            return edges.size();
+        }
+
+        public void addOrUpdateVertex(int vertex, int outgoingEdge, int length) {
+
+            List<Pair<Integer,Integer>> oe = new ArrayList<>();
+            if (outgoingEdge >= 0) {
+                oe.add(new Pair<>(outgoingEdge,length));
+            }
+
+            List<Pair<Integer,Integer>> currValues = edges.putIfAbsent(vertex, oe);
+            if ((currValues != null) && (outgoingEdge >= 0)) {
+                currValues.add( new Pair<>(outgoingEdge,length));
+            }
+
+        }
+        private void addOrUpdateVertex(int vertex, int outgoingEdge) {
+            addOrUpdateVertex(vertex,outgoingEdge,0);
+
+
+        }
+
+    }
+
+
+
+
+
+
+    public static   Graph reverse(Graph in) {
+        Graph retVal = new Graph();
+        in.edges.entrySet().stream().forEach(entry ->  {
+            retVal.addOrUpdateVertex(entry.getKey(), -1);
+            List<Pair<Integer,Integer>> outGoingEdges = entry.getValue();
+            if(outGoingEdges.size() > 0) {
+                for (Pair<Integer,Integer> vertexWithLength : outGoingEdges) {
+                    retVal.addOrUpdateVertex(vertexWithLength.first,entry.getKey(), vertexWithLength.second);
+
+                }
+            }
+        });
+        return retVal;
+    }
+
+    public static Graph updateVertices(Graph graph,  Map<Integer,Integer> keyMap) {
+        Graph retVal = new Graph();
+
+        graph.edges.forEach((key, val) -> {
+           int vertex = keyMap.get(key);
+           if(val.isEmpty()){
+               retVal.addOrUpdateVertex(vertex,-1);
+           } else {
+               val.forEach(edge -> retVal.addOrUpdateVertex(vertex, keyMap.get(edge.first),edge.second));
+           }
+        });
+        return retVal;
+    }
+
+    public static void DFS(Graph graph, Function<Integer,Integer> vIdxGen, List<Boolean> visitedVerticesList, boolean isFinishTimeIteration) {
+
+        int n = graph.numVertices();
+        AtomicInteger t = new AtomicInteger(0);
+        for(int i = n; i >0;i--){
+            final int index = i;
+            //System.out.println("In Loop For Index  "+index);
+            List<Set<Integer>> followersList = new LinkedList<>();
+
+            leaders.putIfAbsent(i, followersList);
+            DFS(graph,vIdxGen.apply(i),t,leaders.get(index),vIdxGen,visitedVerticesList,isFinishTimeIteration);
+            followersList.forEach(followers -> {
+                //System.out.println("# Followers for "+index+ " is "+followers.size());
+                sccSizes.add(followers.size());
+            });
+
+        }
+        //System.out.println("Max Finishing Times after  DFS : " +t.intValue());
+
+    }
+
+    public static void DFS(Graph graph , int vIdx,AtomicInteger t, List<Set<Integer>> followersList,Function<Integer,Integer> vIdxGen, List<Boolean> visitedVerticesList,boolean isFinishTimeIteration) {
+//        boolean[] visitedVertices = new boolean[graph.numVertices()];
+//        System.out.println("Attempting  "+vIdx);
+        Stack<Integer> vertices = new Stack<>();
+        Set<Integer> followers = new LinkedHashSet<>();
+        followersList.add(followers);
+        int cnt =0;
+        if(!visitedVerticesList.get(vIdx-1) ) {
+            vertices.add(vIdx);
+//            followers.add(vIdx);
+//            System.out.println("Processing  "+vIdx+ ", followers: "+followers);
+        }
+
+        while(vertices.size() > 0){
+            cnt++;
+            int idx = vertices.pop();
+            //System.out.println("Popped "+idx);
+            followers.add(idx);
+            //System.out.println("Followers  for "+vIdx+" : "+followers);
+            visitedVerticesList.set(idx-1,  true);
+            List<Pair<Integer,Integer>> edges = graph.edges.get(idx);
+            if(edges == null) {
+
+                if (!finishTimes.containsKey(idx)) {
+                    t.incrementAndGet();
+                    //System.out.println("Setting finishing time for  "+idx+ " as "+t.intValue());
+                    finishTimes.put(idx, t.intValue());
+                    revFinishTimes.put(t.intValue(),idx);
+                }
+                continue;
+            }
+            boolean hasEdgeToBeVisited = false;
+            for (Pair<Integer,Integer> edge : edges) {
+                if(!visitedVerticesList.get(edge.first-1) ) {
+                    hasEdgeToBeVisited = true;
+
+//                    followers.add(edge);
+                    vertices.add(edge.first);
+                    //System.out.println("Added  "+edge+ "for processing. ");
+                }
+                else if(! isFinishTimeIteration) {
+
+                    if (edge.first == vIdx) {
+                        //ONE set of followers / unique SCC completed
+                        // re-init followers
+                        followers = new LinkedHashSet<>();
+                        followers.add(edge.first);
+                        followersList.add(followers);
+
+                    }
+                    else if (followers.contains(vIdx)) {
+                        followers.add(edge.first);
+                    }
+                }
+
+            }
+            if((!hasEdgeToBeVisited) && (!finishTimes.containsKey(idx))){
+                t.incrementAndGet();
+                //System.out.println("Setting finishing time for  "+idx+ " as "+t.intValue());
+                finishTimes.put(idx, t.intValue());
+                revFinishTimes.put(t.intValue(),idx);
+            }
+
+
+        }
+        //System.out.println("Finished the DFS for idx "+vIdx);
+        //System.out.println("Followers for idx "+followersList);
+        Integer[] followersArr = new Integer[followers.size()];
+        Integer[] followersIdx = followers.toArray(followersArr);
+        int followersSize = followersIdx.length;
+        for(int i = followersSize-1;i>=0;i--){
+            //System.out.println("Attempting to set finish time for  "+followersIdx[i]);
+            if(!finishTimes.containsKey(followersIdx[i])) {
+                t.incrementAndGet();
+                //System.out.println("Setting finish time for  "+followersIdx[i]+ " to "+t.intValue());
+                finishTimes.put(followersIdx[i],t.intValue());
+                revFinishTimes.put(t.intValue(),followersIdx[i]);
+            }
+
+        }
+
+
+    }
+
+    public static void rDFS(Graph graph) {
+
+           rDFS(graph, i -> i);
+
+    }
+
+    public static void rDFS(Graph graph, Function<Integer,Integer> vIdxGen) {
+
+        int n = graph.numVertices();
+        AtomicInteger t = new AtomicInteger(0);
+        for(int i = n; i >0;i--){
+            final int index = i;
+            //System.out.println("In Loop For Index  "+index);
+            List<Set<Integer>> followersList = new LinkedList<>();
+
+            leaders.putIfAbsent(i, followersList);
+            rDFS(graph,vIdxGen.apply(i),t,leaders.get(i),vIdxGen);
+            followersList.forEach(followers -> {
+                //System.out.println("# Followers for "+index+ " is "+followers.size());
+                sccSizes.add(followers.size());
+            });
+        }
+
+    }
+    public static void rDFS(Graph graph , int vIdx, AtomicInteger t, List<Set<Integer>> followersList,Function<Integer,Integer> vIdxGen) {
+        //System.out.println("Attempting  "+vIdx);
+        if(visitedVertices[vIdx - 1]) {
+            return;
+        }
+        Set<Integer> followers = null;
+        if(followers.isEmpty()){
+            followers = new LinkedHashSet<>();
+            followersList.add(followers);
+        } else {
+           followers = followersList.get(followersList.size()-1);
+        }
+        followers.add(vIdx);
+        //System.out.println("Processing  "+vIdx+ ", followers: "+followers);
+        visitedVertices[vIdx - 1] = true;
+        List<Pair<Integer,Integer>> ogEdges = null;
+        ogEdges = graph.edges.get(vIdx);
+
+        for( Pair<Integer,Integer> ogEdge : ogEdges) {
+            int ogEdgeToUse = vIdxGen.apply(ogEdge.first);
+            if (!visitedVertices[ogEdgeToUse - 1]) {
+                //System.out.println("Adding follower  "+ogEdgeToUse);
+                followers.add(ogEdgeToUse);
+                rDFS(graph, ogEdgeToUse, t,followersList,vIdxGen);
+            }
+        };
+
+        finishTimes.put(vIdx,t.incrementAndGet());
+        revFinishTimes.put(t.incrementAndGet(),vIdx);
+        //System.out.println("Finishing time for "+vIdx+ " : "+t.intValue());
+
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello Algo!");
+//        week1Assignment();
+        week2Assignment();
+    }
+
+    private static Comparator<Pair<Integer,Integer>> comparatorFor(int vMin) {
+        return (o1, o2) -> {
+
+            int o1Min = o1.second+vMin;
+            int o2Min = o2.second+vMin;
+            if(o1Min < o2Min){
+                return -1;
+            }
+            if(o1Min > o2Min) {
+                return 1;
+            }
+            return 0;
+        };
+    }
+
+    private static void week2Assignment() throws IOException {
+
+        Graph graph = fileContentsAsGraph( "./src/main/resources/dijkstraData.txt");
+
+        // Running Dijkstra Algo with src vertex as 1
+        Map<Integer, Integer> minDistance = new HashMap<>();
+        Set<Integer> X = new HashSet<>();
+        X.add(1);
+        minDistance.put(1,0);
+
+        Set<Integer> Y = graph.edges.values().stream().flatMap(p -> p.stream()).map(k -> k.first).filter(w -> !X.contains(w)).collect(Collectors.toSet());
+        Y.forEach(v-> minDistance.put(v,100000) );
+        while(Y.size() >0) {
+            Iterator<Integer> xI = X.iterator();
+            Set<Pair<Integer, Integer>> yV = new HashSet<>();
+            while (xI.hasNext()) {
+                int v = xI.next();
+                final int vMin = minDistance.get(v);
+                Optional<Pair<Integer, Integer>> minEdge = graph.edges.get(v).stream().filter(p -> Y.contains(p.first)).min(comparatorFor(vMin));
+                minEdge.ifPresent(p -> {
+                            yV.add(new Pair<>(p.first, vMin+p.second));
+                        }
+                );
+
+
+            }
+            yV.stream().min(comparatorFor(0)).ifPresent(v -> {
+                minDistance.put(v.first,v.second);
+                X.add(v.first);
+                Y.remove(v.first);
+            });
+
+        }
+        int[] indices = new int[]{7,37,59,82,99,115,133,165,188,197};
+        System.out.println();
+        for(int i=0;i<indices.length;i++){
+            System.out.print(minDistance.get(indices[i]));
+            if(i<(indices.length -1)) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+
+
+    }
+
+    private static Graph fileContentsAsGraph(String fileName ) throws IOException {
+        Graph graph = new Graph();
+        List<String> fileContents = Files.readAllLines(Paths.get(fileName), Charset.defaultCharset());
+
+        for (String fileContent : fileContents) {
+            if(fileContent.trim().length() == 0){
+                continue;
+            }
+            String[] contents = fileContent.split("\\s+");
+
+            int vertex = Integer.parseInt(contents[0]);
+            for(int i =1;i<contents.length;i++){
+                String[] edgeInfo = contents[i].split(",");
+                if(edgeInfo.length == 2){
+                    graph.addOrUpdateVertex(vertex, Integer.parseInt(edgeInfo[0]),Integer.parseInt(edgeInfo[1]));
+
+                } else if (edgeInfo.length == 1){
+                    graph.addOrUpdateVertex(vertex, Integer.parseInt(edgeInfo[0]));
+                }
+                else {
+                    System.out.println("Invalid Entry in file. No edge info found after the vertex "+vertex+"\n Line: "+fileContent);
+                    continue;
+                }
+                if(graph.edges.get(Integer.parseInt(edgeInfo[0])) == null){
+                    graph.addOrUpdateVertex(Integer.parseInt(edgeInfo[0]), -1);
+                }
+            }
+        }
+        return graph;
+    }
+
+    private static void week1Assignment() throws IOException {
+        Graph graph = fileContentsAsGraph( "./src/main/resources/scc.txt");
+
+        //System.out.println("Orig Graph - "+graph);
+        //System.out.println("Number of vertices - "+graph.numVertices());
+        visitedVertices = new boolean[graph.numVertices()];
+        visitedVerticesList = new ArrayList<>(graph.numVertices());
+        for(int cnt=0;cnt<visitedVertices.length;cnt++){
+            visitedVerticesList.add(Boolean.FALSE);
+        }
+        finishTimes = new TreeMap<Integer,Integer>(Comparator.reverseOrder());
+        revFinishTimes = new HashMap<>();
+        Graph revG = reverse(graph);
+        //System.out.println("Reversed Graph - "+revG);
+        //System.out.println("Number of vertices After Reversal - "+revG.numVertices());
+//        rDFS(revG);
+        DFS(revG,i -> i,visitedVerticesList,true);
+
+        //System.out.println("# Finishing Times after first DFS : " + finishTimes.size());
+        //System.out.println("Finishing Times after first DFS : " + finishTimes);
+        //System.out.println("Reverse Finishing Times after first DFS : " + revFinishTimes);
+        visitedVertices = new boolean[graph.numVertices()];
+        visitedVerticesList.clear();
+        for(int cnt=0;cnt<visitedVertices.length;cnt++){
+            visitedVerticesList.add(Boolean.FALSE);
+        }
+        leaders.clear();
+        sccSizes.clear();
+        Graph updatedKeyGraph = updateVertices(graph, finishTimes);
+        //System.out.println("Updated Keys Graph - "+updatedKeyGraph);
+//        rDFS(updatedKeyGraph);
+        finishTimes.clear();
+        Map<Integer,Integer> revFTToUse = new HashMap<>();
+        revFTToUse.putAll(revFinishTimes);
+        revFinishTimes.clear();
+        DFS(updatedKeyGraph,i -> i,visitedVerticesList,false);
+//        rDFS(updatedKeyGraph);
+
+        //System.out.println("Finished Second pass: " + leaders);
+        System.out.println("The SCCs with their leader: ");
+        leaders.forEach((k, v) -> {
+            if(v.size() > 0) {
+                System.out.print(revFTToUse.get(k) + " -> ");
+                v.forEach(val -> {
+                    val.forEach(val1 -> {
+                        System.out.print(revFTToUse.get(val1) + " ");
+
+                    });
+                    System.out.print(" , ");
+                });
+                System.out.println("");
+            }
+        });
+        sccSizes.sort(Comparator.reverseOrder());
+        System.out.print("SCC Sizes: ");
+        int maxCnt = Math.min(5,graph.edges.size());
+        for(int i=0;i<maxCnt;i++) {
+            System.out.print(sccSizes.get(i));
+            if(i<(maxCnt-1)) {
+                System.out.print(",");
+            }
+        }
+        System.out.println("");
+    }
+
+
+    private static void printArr(String msgPrefix, int[] arr) {
+        System.out.print(msgPrefix);
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < (arr.length - 1)) {
+                System.out.print(",");
+            }
+        }
+        System.out.println("]");
+    }
+
+
+}
